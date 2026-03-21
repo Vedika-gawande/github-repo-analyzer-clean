fd2dbc7ecf3266bebdebb665691c8ef62a71e19d
sanss03 <sanskrutibankar03@gmail.com>
2026-03-21 18:55:57 +0530

    fix: vercel.json in root pointing to backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config({ path: require('path').join(__dirname, '..', '.env') });
 
 const app = express();
 
